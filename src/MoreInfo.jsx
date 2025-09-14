c04bb95840bc63a8e38772bce6bf20fe0841abef
sujay-777 <sujaysudharshan7@gmail.com>
2025-09-14 08:56:56 +0530

    routs from the home page

---
+++ src/MoreInfo.jsx
@@ -0,0 +1,7 @@
+export default function MoreInfo() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black text-white">
+      <h1 className="text-4xl font-bold">Welcome to More Info Page 🚀</h1>
+    </div>
+  );
+}
