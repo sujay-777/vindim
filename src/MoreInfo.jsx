51f630a3208c47766c77789edcd6d4393a84a19a
sujay-777 <sujaysudharshan7@gmail.com>
2025-09-16 10:03:49 +0530

    fix: resized it and bought it to center

--- src/MoreInfo.jsx
+++ src/MoreInfo.jsx
@@ -1,7 +1,85 @@
-export default function MoreInfo() {
+import LiquidEther from "./LiquidEther";
+import RotatingText from './RotatingText'
+import { ArrowRight } from "lucide-react";
+import MagicBento from './pages/MagicBento'
+
+function Moreinfo() {
   return (
-    <div className="flex items-center justify-center h-screen bg-black text-white">
-      <h1 className="text-4xl font-bold">Welcome to More Info Page 🚀</h1>
-    </div>
-  );
+    <>
+      <div style={{ width: '100vw', height: '100vh', position: 'relative' }} className="bg-black">
+        <LiquidEther
+          colors={['#5227FF', '#FF9FFC', '#B19EEF']}
+          mouseForce={20}
+          cursorSize={100}
+          isViscous={false}
+          viscous={30}
+          iterationsViscous={32}
+          iterationsPoisson={32}
+          resolution={0.5}
+          isBounce={false}
+          autoDemo={true}
+          autoSpeed={0.5}
+          autoIntensity={2.2}
+          takeoverDuration={0.25}
+          autoResumeDelay={3000}
+          autoRampDuration={0.6}
+        />
+
+        {/* 👇 Put MagicBento on top */}
+        {/* <div style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", zIndex: 10 }}>
+          <MagicBento
+            textAutoHide={true}
+            enableStars={true}
+            enableSpotlight={true}
+            enableBorderGlow={true}
+            enableTilt={true}
+            enableMagnetism={true}
+            clickEffect={true}
+            spotlightRadius={300}
+            particleCount={12}
+            glowColor="132, 0, 255"
+          />
+        </div> */}
+
+        {/* Background container */}
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            zIndex: 0, // background layer
+          }}
+        >
+          <MagicBento
+            textAutoHide={true}
+            enableStars={true}
+            enableSpotlight={true}
+            enableBorderGlow={true}
+            enableTilt={true}
+            enableMagnetism={true}
+            clickEffect={true}
+            spotlightRadius={300}
+            particleCount={12}
+            glowColor="132, 0, 255"
+          />
+        </div>
+
+        {/* Your header or page content */}
+        <div style={{ position: "relative", zIndex: 1 }}>
+          {/* header, hero, other components */}
+        </div>
+
+
+
+      </div>
+
+    </>
+  )
 }
+
+export default Moreinfo;
